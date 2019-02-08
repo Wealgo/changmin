bd811034ee05f63f2beb39937dc8f1d41c6c5649
ninehundred <ninehundred@outlook.kr>
2019-02-09 01:50:00 +0900

    not working if statement

--- Main.java
+++ Main.java
@@ -1,111 +1,64 @@
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main{
-    public static int[][] map;
-    public static int[][] newMap;
-    public static int num;
+	public static HashMap<Integer, LinkedList<Integer>> graph;
+	public static int[][] output;
+    public static int n;
 	private static Scanner sc;
     public static void main(String[] args) {
+    	graph = new HashMap<>();
     	sc = new Scanner(System.in);
-    	int testcase = sc.nextInt();
-    	
-    	for (int i = 0; i < testcase; i++) {
-			num = 0;
-    		int sero = sc.nextInt();
-			int garo = sc.nextInt();
-			int cabbage = sc.nextInt();
-			map = new int[sero][garo];
-			newMap = new int [sero][garo];
-			
-			for (int j = 0; j < cabbage; j++) {
-				map[sc.nextInt()][sc.nextInt()] = 1;	
-			}
-			for (int k = 0; k < cabbage; k++) {
-				LinkedList<Integer> start = findStart();
-				dfs(start);			
-			}
-			int output = 0;
-			for (int j = 0; j < newMap.length; j++) {
-				for (int j2 = 0; j2 < newMap[j].length; j2++) {
-					if (newMap[j][j2] > output) {
-						output = newMap[j][j2];
+    	n = sc.nextInt();
+    	output = new int [n][n];
+    	for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (sc.nextInt() == 1) {
+					if (graph.containsKey(i)) {
+						graph.get(i).add(j);
+					} else {
+						LinkedList<Integer> list = new LinkedList<>();
+						list.add(j);
+						graph.put(i, list);
 					}
 				}
 			}
-			System.out.println(output);
-        }
-    	sc.close();
+		}
+    	
+    	/**
+    	for (int i = 0; i < output.length; i++) {
+			for (int j = 0; j < output.length; j++) {
+				output[i][j] = dfs(i, j);
+			}
+		}
+    	for (int i = 0; i < output.length; i++) {
+			for (int j = 0; j < output.length; j++) {
+				System.out.print(output[i][j]);
+			}
+			System.out.println();
+		}/**/
+    	Main m = new Main();
+    	System.out.println(graph);
+    	System.out.println("output:"+m.dfs(0, 0));
     }
-    public static void dfs(LinkedList<Integer> position) {
-		
-		if (position.isEmpty()) {
-
+    public int dfs(int start, int end) {
+    	System.out.println("in:"+start);
+    	if (!graph.containsKey(start)) {
+    		return 0;
+		}
+    	//not working this if statement
+		if (graph.get(start).contains(end)) {
+			System.out.println("sibal");
+			return 1;
 		} else {
-		    try {
-		    	newMap[position.getFirst()][position.getLast()] = num;
-		    	//right
-				if (map[position.getFirst()][position.getLast()+1] == 1 && newMap[position.getFirst()][position.getLast()+1] == 0) {
-					LinkedList<Integer> list = new LinkedList<>();
-					list.addAll(position);
-					list.set(1, position.getLast()+1);
-					dfs(list);
-				}
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-		    try {
-				//left
-				if (map[position.getFirst()][position.getLast()-1] == 1 && newMap[position.getFirst()][position.getLast()-1] == 0) {
-					LinkedList<Integer> list = new LinkedList<>();
-					list.addAll(position);
-					list.set(1, position.getLast()-1);
-					dfs(list);
-				}
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-		    try {
-				//up
-				if (map[position.getFirst()-1][position.getLast()] == 1 && newMap[position.getFirst()-1][position.getLast()] == 0) {
-					LinkedList<Integer> list = new LinkedList<>();
-					list.addAll(position);
-					list.set(0, position.getFirst()-1);
-					dfs(list);
-				}
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-		    try {
-				//down
-				if (map[position.getFirst()+1][position.getLast()] == 1 && newMap[position.getFirst()+1][position.getLast()] == 0) {
-					
-					LinkedList<Integer> list = new LinkedList<>();
-					list.addAll(position);
-					list.set(0, position.getFirst()+1);
-					dfs(list);
-				}
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-		    
-		}
-	}
-    
-    public static LinkedList<Integer> findStart() {
-    	num++;
-		LinkedList<Integer> start = new LinkedList<>();
-    	for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				if (map[i][j] == 1 && newMap[i][j] == 0) {
-					start.add(i);
-					start.add(j);
-					return start;
-				}
+			for (int i = 0; i < graph.get(start).size(); i++) {
+					dfs(graph.get(start).get(i), end);
 			}
 		}
-    	return start;
-	}
+		return 0;
+    }
 
 }
 
