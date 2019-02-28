fd5e3298155689ca0d0a7f85ad981587f62b4733
ninehundred <ninehundred@outlook.kr>
2019-03-01 03:22:08 +0900

    done

--- boj/11403.java
+++ boj/11403.java
@@ -1,5 +1,4 @@
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -8,70 +7,51 @@ public class Main{
     public static int n;
     public static int[][] oldMap;
     public static int[][] newMap;
-    public static HashMap<Integer, LinkedList<Integer>> map;
-    
+    public static int[] visited;
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	int size = sc.nextInt();
-    	map = new HashMap<>();
+
     	oldMap = new int [size][size];
     	newMap = new int [size][size];
-    	LinkedList<Integer> list;
-    	for (int i = 0; i < oldMap.length; i++) {	
-			list = new LinkedList<>();
-    		for (int j = 0; j < oldMap.length; j++) {
-				oldMap[i][j] = sc.nextInt();
-				if (oldMap[i][j] == 1) {
-					list.add(j);
-				}
-			}
-    		map.put(i, list);
-		}
-
+    	visited = new int [size];
     	for (int i = 0; i < oldMap.length; i++) {
-    		sibal = i;
 			for (int j = 0; j < oldMap.length; j++) {
-				list = new LinkedList<>();
-				dfs(i, j, list);
+				oldMap[i][j] = sc.nextInt();
 			}
+		}	
+    	for (int i = 0; i < oldMap.length; i++) {
+			dfs(i);
+			/**
+			 * 여기 2중 포문이 핵심.
+			 * 즉, newMap에서 받아오는것보다는,
+			 * 전역변수 visited배열을 활용해서 처리하자.
+			 */
+			for (int j = 0; j < newMap.length; j++) {
+				newMap[i][j] = visited[j];
+			}
+			//
+			visited = new int[size];
 		}
-
     	for (int i = 0; i < newMap.length; i++) {
 			for (int j = 0; j < newMap.length; j++) {
-				System.out.print(newMap[i][j]+" ");
+				System.out.print(newMap[i][j] + " ");
 			}
 			System.out.println();
 		}
     }
-    public static int sibal;
-    public static void dfs(int start, int end, LinkedList<Integer> duplist) {
-    	if (map.get(start).contains(end)) {
-			newMap[sibal][end] = 1;
-		} else {
-			if (checkDu(duplist)) {
-				duplist.removeLast();
-			} else {
-				for (int i = 0; i < map.get(start).size(); i++) {
-			    	duplist.add(map.get(start).get(i));
-
-					dfs(map.get(start).get(i), end, duplist);
-				}
+    public static void dfs(int location) {
+    	for (int i = 0; i < oldMap.length; i++) {
+			if (oldMap[location][i] == 1 && visited[i] == 0) {
+				visited[i] = 1;
+				/**
+				 * 내가 했던 실수.
+				 * 여기서 newMap이라는 전역변수를 사용해서 바로 1을 넣어버리는 짓은 하지말자
+				 * ex) newMap[location][i] = 1;
+				 * location 변수가 들어올때마다 바뀌므로 출력해보면 인풋과 똑같은 값들을 아웃풋으로 보내게 된다.
+				 */
+				dfs(i);
 			}
 		}
-    }
-    public static boolean checkDu(LinkedList<Integer> list) {
-    	if (list.isEmpty() || list.size() == 1) {
-			return false;
-		} else {
-			LinkedList<Integer> nimi = new LinkedList<>();
-			nimi.addAll(list);
-			Collections.sort(nimi);
-			for (int i = 0; i < nimi.size()-1; i++) {
-				if (nimi.get(i).equals(nimi.get(i+1))) {
-					return true;
-				}
-			}
-		}
-    	return false;
     }
 }
