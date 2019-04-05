fe0e7824b5f725806ad7579e95021213b778f19a
ninehundred <ninehundred@outlook.kr>
2019-04-05 12:03:59 +0900

    done

--- boj/1697.java
+++ boj/1697.java
@@ -1,39 +1,44 @@
 
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-/**
- * 시간초과.
- * 1. 범위가 0부터 100000까지인데, 십만 이상의 숫자가 리스트에 추가되고, 계산됨.
- * 2. 0 이하의 숫자들도 리스트에 추가되고, 계산되어짐.
- * 
- * @author quadcore
- *
- */
+
 public class Main{
-	public static int time = 1;
-    public static void main(String[] args) throws IOException {
+	public static int time = 0;
+	public static int[] visit = new int [100001];
+    public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	int subin = sc.nextInt();
     	int sister = sc.nextInt();
-    	
+    	sc.close();
     	bfs(subin, sister);
-    	System.out.println(time-1);
+    	
     }
     
     public static void bfs(int subin, int sister) {
     	LinkedList<Integer> list = new LinkedList<>();
     	list.add(subin);
-    	while (!list.contains(sister)) {
-    		LinkedList<Integer> tmp = list;
-    		list = new LinkedList<>();
-    		for (int i = 0; i < tmp.size(); i++) {
-				list.add(tmp.get(i)+1);
-				list.add(tmp.get(i)-1);
-				list.add(tmp.get(i)*2);
+    	visit[subin] = 1;
+		while (!list.isEmpty()) {
+			subin = list.poll();
+			if (subin == sister) {
+				break;
 			}
-    		time++;
+			//범위가 십만을 넘는것과 이미 방문한 위치는 스킵한다.
+			if ( subin+1 <= 100000 && visit[subin+1] == 0) {
+				list.add(subin+1);
+				visit[subin+1] = visit[subin]+1;
+			}
+			//범위가 0이하로 내려가는 것과 이미 방문한 위치는 스킵한다.
+			if ( subin-1 >= 0 && visit[subin-1] == 0) {
+				list.add(subin-1);
+				visit[subin-1] = visit[subin]+1;
+			}
+			if ( subin*2 <= 100000 && visit[subin*2] == 0) {
+				list.add(subin*2);
+				visit[subin*2] = visit[subin]+1;
+			}
 		}
+		System.out.println(visit[sister]-1);
     }
 }
