2e6dd2a8eedf21d49d93e26a6f230157a957a6c2
nine-hundred <ninehundred@outlook.kr>
2020-06-28 21:00:20 +0900

    done

---
+++ boj/2776.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+/**
+ * EZ
+ * @author quadcore
+ */
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		HashMap<String, Boolean> map;
+		StringBuilder sb;
+		int testcase = Integer.parseInt(br.readLine());
+		for (int t = 0; t < testcase; t++) {
+			map = new HashMap<>();
+			sb = new StringBuilder();
+			int n = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				map.put(st.nextToken(), true);
+			}
+			int m = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < m; i++) {
+				if (map.containsKey(st.nextToken())) sb.append(1+"\n");
+				else sb.append(0+"\n");
+			}
+			System.out.print(sb.toString());
+		}
+	}
+}
