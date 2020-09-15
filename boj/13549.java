d718db23f03ed7fe9c4c15481178c82531197def
nine-hundred <ninehundred@outlook.kr>
2020-09-15 20:47:42 +0900

    failed

---
+++ boj/13549.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+/**
+ * not EZ
+ * @author quadcore
+ */
+class Main {
+	public static int n, sister;
+	public static int time;
+	public static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		sister = Integer.parseInt(st.nextToken());
+		visited = new boolean[100002];
+		time = 0;
+		bfs(n);
+		System.out.println(time);
+	}
+
+	public static void bfs(int start) {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(start);
+		while (!list.isEmpty()) {
+			int size = list.size();
+			for (int i = 0; i < size; i++) {
+				int tmp = list.poll();
+				while (true) {
+					if (tmp > sister) break;
+					list.addLast(tmp);
+					tmp = tmp * 2;
+				}
+			}
+			size = list.size();
+			for (int i = 0; i < size; i++) {
+				int subin = list.poll();
+				if (subin == sister) return;
+				if (subin > 100000) continue;
+				if (subin < 0) continue;
+				visited[subin] = true;
+				if (subin-1 >= 0 && !visited[subin-1]) list.add(subin-1);
+				if (!visited[subin+1] && subin+1 <= 100000) list.add(subin+1);
+			}
+			time++;
+		}
+	}
+}
