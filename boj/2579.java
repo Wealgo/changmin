4c42460c367cd42cfa16feca98ab8c808c4e9c6b
nine-hundred <ninehundred@outlook.kr>
2021-01-22 23:58:39 +0900

    done

--- boj/2579.java
+++ boj/2579.java
@@ -1,33 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 /**
  * EZ
  * @author quadcore
  */
 class Main {
-	public static LinkedList<Integer> list = new LinkedList<>();
 	public static int result = 0;
 	public static int size;
+	public static int[] stair, dp;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		size = Integer.parseInt(br.readLine());
-		for (int i = 0; i < size; i++) {
-			list.add(Integer.parseInt(br.readLine()));
+		dp = new int[301];
+		stair = new int[301];
+//		for (int i = 0; i < dp.length; i++) dp[i] = -1;
+		
+		for (int i = 1; i <= size; i++) {
+			stair[i] = Integer.parseInt(br.readLine());
 		}
-		list.add(0);
-		list.add(0);
-		go(0,0,0);
-		System.out.println(result);
+		dp[1] = stair[1];
+		dp[2] = stair[1] + stair[2];
+		dp[3] = Math.max(stair[1] + stair[3], stair[2] + stair[3]);
+		for (int i = 4; i <= size; i++) {
+			dp[i] = Math.max(dp[i-2] + stair[i], dp[i-3] + stair[i] + stair[i-1]);
+		}
+		System.out.println(dp[size]);
 	}
-	public static void go(int stage, int chain, int score) {
-		if (chain >= 3) return;
-		if (stage > size) return;
-		if (stage == size) result = Math.max(result, score+list.getLast());
-		int _score = score + list.get(stage);
-		go(stage+1, chain+1, _score);
-		go(stage+2, 0, _score);
-	}
+	
 }
